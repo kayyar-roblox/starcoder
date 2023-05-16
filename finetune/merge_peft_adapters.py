b5d0ffc3c24f6f65850262ba8fdf1bedad187513
Kartik Ayyar <kayyar@roblox.com>
2023-05-16 11:06:57 -0700

    Reformat.

--- finetune/merge_peft_adapters.py
+++ finetune/merge_peft_adapters.py
@@ -5,13 +5,17 @@ import torch
 import os
 import argparse
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--base_model_name_or_path", type=str, default="bigcode/large-model")
+    parser.add_argument(
+        "--base_model_name_or_path", type=str, default="bigcode/large-model"
+    )
     parser.add_argument("--peft_model_path", type=str, default="/")
     parser.add_argument("--push_to_hub", action="store_true", default=True)
 
     return parser.parse_args()
+
 
 def main():
     args = get_args()
@@ -19,7 +23,7 @@ def main():
     base_model = AutoModelForCausalLM.from_pretrained(
         args.base_model_name_or_path,
         return_dict=True,
-        torch_dtype=torch.float16 
+        torch_dtype=torch.float16,
     )
 
     model = PeftModel.from_pretrained(base_model, args.peft_model_path)
@@ -30,12 +34,21 @@ def main():
 
     if args.push_to_hub:
         print(f"Saving to hub ...")
-        model.push_to_hub(f"{args.base_model_name_or_path}-merged", use_temp_dir=False, private=True)
-        tokenizer.push_to_hub(f"{args.base_model_name_or_path}-merged", use_temp_dir=False, private=True)
+        model.push_to_hub(
+            f"{args.base_model_name_or_path}-merged",
+            use_temp_dir=False,
+            private=True,
+        )
+        tokenizer.push_to_hub(
+            f"{args.base_model_name_or_path}-merged",
+            use_temp_dir=False,
+            private=True,
+        )
     else:
         model.save_pretrained(f"{args.base_model_name_or_path}-merged")
         tokenizer.save_pretrained(f"{args.base_model_name_or_path}-merged")
         print(f"Model saved to {args.base_model_name_or_path}-merged")
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     main()

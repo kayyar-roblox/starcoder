d0b6b77ff69c23370938fc96c348908a398c3513
Kartik Ayyar <kayyar@roblox.com>
2023-05-17 12:58:52 -0700

    Change merge checkpoint script to use absl.

--- finetune/merge_peft_adapters.py
+++ finetune/merge_peft_adapters.py
@@ -1,54 +1,48 @@
+import os
+import torch
+from absl import app, flags
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
-import torch
 
-import os
-import argparse
+FLAGS = flags.FLAGS
 
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--base_model_name_or_path", type=str, default="bigcode/large-model"
-    )
-    parser.add_argument("--peft_model_path", type=str, default="/")
-    parser.add_argument("--push_to_hub", action="store_true", default=True)
-
-    return parser.parse_args()
+flags.DEFINE_string(
+    "base_model_name_or_path", "bigcode/large-model", "Base model name or path."
+)
+flags.DEFINE_string("peft_model_path", "/", "PEFT model path.")
+flags.DEFINE_bool("push_to_hub", True, "Push to hub.")
 
 
-def main():
-    args = get_args()
-
+def main(argv):
     base_model = AutoModelForCausalLM.from_pretrained(
-        args.base_model_name_or_path,
+        FLAGS.base_model_name_or_path,
         return_dict=True,
         torch_dtype=torch.float16,
     )
 
-    model = PeftModel.from_pretrained(base_model, args.peft_model_path)
+    model = PeftModel.from_pretrained(base_model, FLAGS.peft_model_path)
     model = model.merge_and_unload()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    tokenizer = AutoTokenizer.from_pretrained(args.base_model_name_or_path)
+    tokenizer = AutoTokenizer.from_pretrained(FLAGS.base_model_name_or_path)
 
-    if args.push_to_hub:
+    if FLAGS.push_to_hub:
         print(f"Saving to hub ...")
         model.push_to_hub(
-            f"{args.base_model_name_or_path}-merged",
+            f"{FLAGS.base_model_name_or_path}-merged",
             use_temp_dir=False,
             private=True,
         )
         tokenizer.push_to_hub(
-            f"{args.base_model_name_or_path}-merged",
+            f"{FLAGS.base_model_name_or_path}-merged",
             use_temp_dir=False,
             private=True,
         )
     else:
-        model.save_pretrained(f"{args.base_model_name_or_path}-merged")
-        tokenizer.save_pretrained(f"{args.base_model_name_or_path}-merged")
-        print(f"Model saved to {args.base_model_name_or_path}-merged")
+        model.save_pretrained(f"{FLAGS.base_model_name_or_path}-merged")
+        tokenizer.save_pretrained(f"{FLAGS.base_model_name_or_path}-merged")
+        print(f"Model saved to {FLAGS.base_model_name_or_path}-merged")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(main)
